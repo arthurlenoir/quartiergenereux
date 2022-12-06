1f62122ab78d4f27063e6ffb260e81fe6c763672
Arthur Lenoir <lenoir.arthur@gmail.com>
2022-12-06 10:36:32 +0100

    fix: export sitemap in public folder

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://quartiergenereux.fr',
-    generateRobotsTxt: true,
-    outDir: 'out'
+    generateRobotsTxt: true
 }
