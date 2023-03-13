14bdcfe47d28065fd6f0e1b98115b90aca4c660f
Arthur Lenoir <lenoir.arthur@gmail.com>
2023-03-13 11:33:50 +0100

    fix: export config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@
 const withTM = require('next-transpile-modules')(['@piwikpro/next-piwik-pro'])
 
 const nextConfig = {
+  images: {
+    unoptimized: true
+  },
   reactStrictMode: true,
   swcMinify: true
 }
