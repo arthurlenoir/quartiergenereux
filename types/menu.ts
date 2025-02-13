294ce3944266c996b6992ead90f0032d52439f5c
Arthur Lenoir <lenoir.arthur@gmail.com>
2025-02-13 18:06:26 +0100

    feat: enable to display menu with sub levels

--- types/menu.ts
+++ types/menu.ts
@@ -1,35 +1,49 @@
 export type MenuItem = {
-    id: string,
-    label: string | null,
-    description: string | null,
-    order: number,
-    title: string,
-    uri: string,
-    parentId?: string | null,
-
-}
+  id: string;
+  label: string | null;
+  description: string | null;
+  order: number;
+  title: string;
+  uri: string;
+  parentId?: string | null;
+  children?: MenuItem[];
+};
 
 export type Menu = {
-    id: string,
-    name: string,
-    menuItems: MenuItem[],
-}
+  id: string;
+  name: string;
+  menuItems: MenuItem[];
+};
 
 type WPMenuItems = {
-    nodes: MenuItem[],
-}
+  nodes: MenuItem[];
+};
 
 type WPMenu = {
-    id: string,
-    name: string,
-    menuItems: WPMenuItems,
-}
+  id: string;
+  name: string;
+  menuItems: WPMenuItems;
+};
 
-
-export const parseMenus = (wpMenu: WPMenu): Menu => (
-    {
-        id: wpMenu.id,
-        name: wpMenu.name,
-        menuItems: wpMenu.menuItems.nodes
+const parseMenuItems = (menuItems: MenuItem[]) => {
+  const menuItemByParentId = menuItems.reduce((acc, menuItem) => {
+    if (menuItem.parentId) {
+      acc[menuItem.parentId] = [...(acc[menuItem.parentId] ?? []), menuItem];
     }
-)
+    return acc;
+  }, []);
+  return menuItems.reduce((acc: MenuItem[], menuItem) => {
+    if (menuItem.parentId) return acc;
+    if (menuItemByParentId[menuItem.id]) {
+      menuItem.children = menuItemByParentId[menuItem.id];
+    }
+    acc.push(menuItem);
+    return acc;
+  }, []);
+};
+
+export const parseMenus = (wpMenu: WPMenu): Menu => ({
+  id: wpMenu.id,
+  name: wpMenu.name,
+  menuItems: parseMenuItems(wpMenu.menuItems.nodes),
+});
--- utils/fetchMenu.ts
+++ utils/fetchMenu.ts
@@ -1,13 +1,13 @@
 import { parseMenus } from "../types/menu";
 
 export const fetchMenu = async () => {
-    const res = await fetch('http://quartiu.cluster031.hosting.ovh.net/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            query: `
+  const res = await fetch("http://quartiu.cluster031.hosting.ovh.net/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
         {
           generalSettings {
               url, title, description
@@ -31,12 +31,15 @@ export const fetchMenu = async () => {
           }
         }
     `,
-        }),
-    })
-    const { data: {
-        generalSettings: { title, description },
-        menus: { nodes: wpMenus }
-    } } = await res.json();
-    const menu = wpMenus.length > 0 ? parseMenus(wpMenus[0]) : null;
-    return { title, description, menu };
-}
+    }),
+  });
+  const {
+    data: {
+      generalSettings: { title, description },
+      menus: { nodes: wpMenus },
+    },
+  } = await res.json();
+  console.log("WP MENUS", JSON.stringify(wpMenus[0].menuItems.nodes));
+  const menu = wpMenus.length > 0 ? parseMenus(wpMenus[0]) : null;
+  return { title, description, menu };
+};
